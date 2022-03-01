f6bff09ebb368beb36bc6e5df4bbfe3853310e2c
Justin Thomas <justmas@gmail.com>
2022-02-28 21:36:00 -0600

    Version 0 working with default creds

--- s3presigned-plugin.php
+++ s3presigned-plugin.php
@@ -1,23 +1,93 @@
 <?php
 /*
 Plugin Name: s3presigned
-Plugin URI: 
+Plugin URI:
 Description: Generate pre-signed url with shortcode
 Author: jthomas
 Author URI: https://github.com/twoquarterrican
 Version: 0.1
-*/
+ */
+
+// require_once 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
 
 add_shortcode('s3presigned', 's3presigned_shortcode');
-function s3presigned_shortcode( $atts = [], $content = null) {
-    if ( 
-        defined( 'S3_UPLOADS_BUCKET' )
-        && defined( 'S3_UPLOADS_REGION' )
-        && defined( 'S3_UPLOADS_KEY' )
-        && defined( 'S3_UPLOADS_SECRET' )
+
+/**
+ * The [s3presigned] shortcode.
+ *
+ * Accepts an href title, downloadName and will display a link
+ * whose href is a presigned s3 url of the given href.
+ *
+ * @param array  $atts    Shortcode attributes. Default empty.
+ * @param string $content Shortcode content. Default 'download'.
+ * @param string $tag     Shortcode tag (name). Default empty.
+ * @return string Shortcode output.
+ */
+function s3presigned_shortcode($atts = [], $content = 'download', $tag = '')
+{
+    // sanity check #1
+    if (
+        !defined('S3PRESIGNED_BUCKET')
+        || !defined('S3PRESIGNED_REGION')
     ) {
-        return 'setup present';
-    } else {
-        return 'missing setup';
+        $result = '<div hidden>s3presigned plugin not set up. Check if you put';
+        $result .= " define( 'S3PRESIGNED_BUCKET', '...') ";
+        $result .= " and define ( 'S3PRESIGNED_REGION', '...') ";
+        $result .= " in wp-config</div>";
+        $result .= $content;
+        return result;
+    }
+
+    // normalize attribute keys, lowercase
+    $atts = array_change_key_case((array) $atts, CASE_LOWER);
+
+    // override default attributes with user attributes
+    $wporg_atts = shortcode_atts(
+        array(
+            'title' => 'Temporary link',
+            'downloadName' => '',
+        ), $atts, $tag
+    );
+
+    // sanity check #2
+    if (empty($atts['href'])) {
+        return '<div hidden>s3presigned shortcode found no href</div>' . $content;
+    }
+
+    try {
+        $s3Client = new Aws\S3\S3Client([
+            'region' => S3PRESIGNED_REGION,
+            'version' => '2006-03-01',
+            // using default credentials
+        ]);
+
+        $cmd = $s3Client->getCommand('GetObject', [
+            'Bucket' => S3PRESIGNED_BUCKET,
+            'Key' => $atts['href'],
+        ]);
+
+        $request = $s3Client->createPresignedRequest($cmd, '+20 minutes');
+        $presignedUrl = (string) $request->getUri();
+
+        $a = '<a href="' . $presignedUrl . '"';
+        $a .= ' title="' . $atts['title'] . '"';
+        if (empty($atts['downloadName'])) {
+            $a .= ' download>';
+        } else {
+            $a .= ' download="' . $atts['downloadName'] . '">';
+        }
+        if (empty($content)) {
+            $a .= 'download</a>';
+        } else {
+            $a .= $content . '</a>';
+        }
+        $a .= '</a>';
+        return $a;
+    } catch (Exception $ex) {
+        $result = '<div hidden>s3presigned shortcode failed to generate presigned url';
+        $result .= $ex . '</div>' . $content;
+        return $result;
     }
 }
